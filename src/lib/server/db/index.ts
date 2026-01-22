bc159814b1e4c873d46a6d9733ea34b26389dacc
Syukron Maulana <s.ssyukronmaulana@gmail.com>
2026-01-22 12:13:59 +0700

    fix: set default mode for drizzle ORM in database connection

--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -7,5 +7,5 @@ const { DATABASE_URL } = env;
 
 const poolConnection = mysql.createPool(DATABASE_URL);
 
-export const db = drizzle(poolConnection, { schema });
+export const db = drizzle(poolConnection, { schema, mode: "default" });
 export type DB = typeof db;
